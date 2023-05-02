d1add5ebc1fa7f89086d97e2b00d9652536f8da5
kunal <kunalaage21@gmail.com>
2023-05-02 12:22:30 +0530

    feat: make-filter record api

--- index.js
+++ index.js
@@ -64,14 +64,26 @@ app.get("/product/:id", async (req, resp) => {
   }
 });
 
-app.put("/product/:id",async(req,resp)=>{
+app.put("/product/:id", async (req, resp) => {
   let result = await Product.updateOne(
-    {_id:req.params.id},
+    { _id: req.params.id },
     {
-      $set:req.body
+      $set: req.body,
     }
-  )
-  resp.send(result)
-})
+  );
+  resp.send(result);
+});
+
+//api for filter data
+app.get("/search/:key", async (req, resp) => {
+  let result = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ],
+  });
+  resp.send(result);
+});
 
 app.listen(5000);
